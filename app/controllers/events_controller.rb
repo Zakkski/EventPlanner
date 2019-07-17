c5ada6ba93afc1744f8390ac9560f266e3f8d249
Zachary Kwiecinski <Zachary.Kwiecinski@gmail.com>
2019-07-17 18:15:53 -0400

    add basic paths for events and players

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,46 @@
 class EventsController < ApplicationController
+  def index
+    @events = current_user.events
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    @players = @event.players
+    # raise
+    @new_player = Player.new(event: @event)
+  end
+
   def new
     @event = Event.new
   end
 
   def create
+    event = Event.new(event_params)
+    event.user = current_user
+
+    if event.save!
+      redirect_to event
+    else
+      render :new
+    end
+  end
+
+  def update
+    event = Event.find(params[:id])
+    event.open = !event.open
+    event.save
+    redirect_to event_path(event)
+  end
+
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
+    render :index
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :date)
   end
 end
