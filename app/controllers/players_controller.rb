694d7b41f954ea143a644b84fd379be22a5165cb
Zachary Kwiecinski <Zachary.Kwiecinski@gmail.com>
2019-07-18 14:26:53 -0400

    add score changing buttons (no ajax)

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,16 +1,33 @@
 class PlayersController < ApplicationController
+
+  def new
+    @event = Event.find(params[:event_id])
+    @player = Player.new
+    @countries = []
+    CSV.foreach('./app/assets/data/countries.csv') do |row|
+      @countries << row[0]
+    end
+  end
+
   def create
     player = Player.new(player_params)
     player.country_code = kabab_case(player.country_code)
-    event = Event.find(player.event_id)
+    event = Event.find(params[:event_id])
+    player.event = event
     player.save! if event.open
     redirect_to event_path(event)
+  end
+
+  def update
+    player = Player.find(params[:id])
+    player.update_attributes(player_params)
+    redirect_to event_path(Event.find(params[:event_id]))
   end
 
   private
 
   def player_params
-    params.require(:player).permit(:name, :title, :country_code, :event_id)
+    params.require(:player).permit(:name, :title, :country_code, :event_id, :score)
   end
 
   def kabab_case(string)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :events, only: [:index, :show, :new, :create, :update, :destroy]
-  resources :players, only: [:create]
+  resources :events, only: [:index, :show, :new, :create, :update, :destroy] do
+    resources :players, only: [:new, :create, :update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
